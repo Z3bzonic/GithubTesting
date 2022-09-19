1dfdf3c0be8cf846ecf7efde680f390e48e126a0
Bart Roels <roelsbart@gmail.com>
2022-09-19 21:21:57 +0200

    main terug

---
+++ GitTesting/AnotherDeveloper.cs
@@ -0,0 +1,13 @@
+﻿namespace GitTesting
+{
+    internal class AnotherDeveloper
+    {
+        public void OtherMethod()
+        {
+            var nogiet = 3 + 3;
+
+            var otherDev = new OtherDeveloper();
+            var ditCijfer = 1050.6M;
+        }
+    }
+}
--- GitTesting/Program.cs
+++ GitTesting/Program.cs
@@ -1,4 +1,12 @@
-﻿void InputPlusOne()
+﻿Test();
+
+void Test()
+{
+    var qzefrg = 5.2345f + 5.2345f;
+    Console.WriteLine(qzefrg);
+}
+
+void InputPlusOne()
 {
     var result = 1 + 1;
 }
